19e871221e0405d0eb7d8a8b17c686d1be40976c
RyanDwitama <76994573+RyanDwitama@users.noreply.github.com>
2022-11-28 12:35:13 +0700

    Add files via upload

--- resources/views/culinary.blade.php
+++ resources/views/culinary.blade.php
@@ -5,95 +5,79 @@
 @section('content')
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 body {font-family: Arial, Helvetica, sans-serif;}
 
-#myImg {
-  border-radius: 5px;
-  cursor: pointer;
-  transition: 0.3s;
+<style>
+
+body{
+    background-size:cover;
+    font-family:Arial;
 }
 
-#myImg:hover {opacity: 0.7;}
-
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+.wrap{
+    width:80%;
+    margin:auto;
+    background:#fff;
+    border-radius: 3%;
+    padding-top: 2%;
+    margin-top: 5%;
 }
 
-/* Modal Content (image) */
-.modal-content {
-  margin: auto;
-  display: block;
-  width: 80%;
-  max-width: 700px;
+form{
+    margin:2%;
 }
 
-/* Caption of Modal Image */
-#caption {
-  margin: auto;
-  display: block;
-  width: 80%;
-  max-width: 700px;
-  text-align: center;
-  color: #ccc;
-  padding: 10px 0;
-  height: 150px;
+input{
+    width:100%;
+    padding:10px;
+    box-sizing:border-box;
+    margin-bottom: 10px;
+    border:none;
+    outline:none;
+    border:1px solid gray;
+    font-size:15px;
 }
 
-/* Add Animation */
-.modal-content, #caption {  
-  -webkit-animation-name: zoom;
-  -webkit-animation-duration: 0.6s;
-  animation-name: zoom;
-  animation-duration: 0.6s;
+.openbtn{
+    background:#f1f1f1;
+    border:none;
+    padding:10px 15px;
+    font-size:20px;
+    cursor:pointer;
+    float:right;
 }
 
-@-webkit-keyframes zoom {
-  from {-webkit-transform:scale(0)} 
-  to {-webkit-transform:scale(1)}
+.overlay{
+    height:100%;
+    width:100%;
+    display:none;
+    position:fixed;
+    z-index:1;
+    top:0;
+    left:0;
+    background:rgba(0,0,0,0.8);
 }
 
-@keyframes zoom {
-  from {transform:scale(0)} 
-  to {transform:scale(1)}
+.closebtn{
+    position:absolute;
+    top:0%;
+    right:10%;
+    font-size:60px;
+    cursor:pointer;
+    color: red;
 }
 
-/* The Close Button */
-.close {
-  position: absolute;
-  top: 15px;
-  right: 35px;
-  color: #f1f1f1;
-  font-size: 40px;
-  font-weight: bold;
-  transition: 0.3s;
+.rating{
+    position: absolute;
+    top: 14%;
+    right: 15%;
 }
 
-.close:hover,
-.close:focus {
-  color: #bbb;
-  text-decoration: none;
-  cursor: pointer;
-}
-
-/* 100% Image Width on Smaller Screens */
-@media only screen and (max-width: 700px){
-  .modal-content {
-    width: 100%;
-  }
-}
 </style>
+
+
 
 <div style="margin-left: 5px; margin-top: 100px">  
   <ul style="display: flex">
@@ -184,7 +168,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
             <div style="width: 40%; float: left;">
                 <form class="example" action="/action_page.php" style="margin:auto;">
                     <input type="text" placeholder="Search.." name="search2">
-                    <button type="submit"><i class="fa fa-search"></i></button>
+                    <button type="search"><i class="fa fa-search" style="font-size: 225%"></i></button>
                 </form>
             </div>
           </h3>
@@ -211,16 +195,16 @@ body {font-family: Arial, Helvetica, sans-serif;}
                 <b style="font-size: 100%;">Roasted Arparagus with Pamersan</b>
             </div>
 
-            <div style="width: 25%; float: left; margin-top: 2%; margin-right: 12%;">
-                <a id="myImg" target="_blank" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 2%;">More</a>
+            <div style="width: 25%; float: left; margin-top: 1%; margin-bottom: 1%; margin-right: 12%">
+                <button class = "openbtn" onclick ="openForm()" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 1%; float:left; margin-bottom: 4%">More</button>
             </div>
             
-            <div style="width: 25%; float: left; margin-top: 2%; margin-right: 12%;">
-                <a href="https://www.w3schools.com" target="_blank" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 2%;">More</a>
+            <div style="width: 25%; float: left; margin-top: 1%; margin-bottom: 1%; margin-right: 12%;">
+                <button class = "openbtn" onclick ="openForm()" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 1%; float:left; margin-bottom: 4%">More</button>
             </div>
 
-            <div style="width: 25%; float: left; margin-top: 2%; margin-bottom: 3%;">
-                <a href="https://www.w3schools.com" target="_blank" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 2%;">More</a>
+            <div style="width: 25%; float: left; margin-top: 1%; margin-bottom: 1%">
+                <button class = "openbtn" onclick ="openForm()" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 1%; float:left; margin-bottom: 4%">More</button>
             </div>
         </div>
 
@@ -241,16 +225,16 @@ body {font-family: Arial, Helvetica, sans-serif;}
                 <b style="font-size: 100%;">Roasted Arparagus with Pamersan</b>
             </div>
 
-            <div style="width: 25%; float: left; margin-top: 2%; margin-right: 12%;">
-                <a href="https://www.w3schools.com" target="_blank" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 2%;">More</a>
+            <div style="width: 25%; float: left; margin-top: 1%; margin-bottom: 1%; margin-right: 12%;">
+                <button class = "openbtn" onclick ="openForm()" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 1%; float:left; margin-bottom: 4%">More</button>
             </div>
             
-            <div style="width: 25%; float: left; margin-top: 2%; margin-right: 12%;">
-                <a href="https://www.w3schools.com" target="_blank" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 2%;">More</a>
+            <div style="width: 25%; float: left; margin-top: 1%; margin-bottom: 1%; margin-right: 12%;">
+                <button class = "openbtn" onclick ="openForm()" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 1%; float:left; margin-bottom: 4%">More</button>
             </div>
 
-            <div style="width: 25%; float: left; margin-top: 2%; margin-bottom: 3%;">
-                <a href="https://www.w3schools.com" target="_blank" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 2%;">More</a>
+            <div style="width: 25%; float: left; margin-top: 1%; margin-bottom: 1%">
+                <button class = "openbtn" onclick ="openForm()" style="text-decoration: none; border-style: groove; border-color: green; border-radius: 10px; color:green; padding: 1%; float:left; margin-bottom: 4%">More</button>
             </div>
         </div>
 
@@ -292,41 +276,51 @@ body {font-family: Arial, Helvetica, sans-serif;}
  
 <!-------------------------------------popup gambar-------------------------->
 
-<h2>Image Modal</h2>
-<p>In this example, we use CSS to create a modal (dialog box) that is hidden by default.</p>
-<p>We use JavaScript to trigger the modal and to display the current image inside the modal when it is clicked on. Also note that we use the value from the image's "alt" attribute as an image caption text inside the modal.</p>
 
-<img id="myImg" src="gambar/contohgambar.png" alt="Snow" style="width:100%;max-width:300px">
+<div id ="myOverlay" class ="overlay">
 
-<!-- The Modal -->
-<div id="myModal" class="modal">
-  <span class="close">&times;</span>
-  <img class="modal-content" id="img01">
-  <div id="caption"></div>
+    <span class ="closebtn" onclick = "closeForm()" title ="Close"> X </span>
+    <span class ="rating" title="Rating">
+        <div style="font-size: 200%">
+            Ini Rating <i class="fa fa-thumbs-up"></i>
+        </div>
+    </span>
+
+    <div class = "wrap">
+        <form>
+            <div style="text-align: center">
+                <b style="color:black; font-size: 200%">Menu Description</b>
+            </div>
+
+            <img src="gambar/contohgambar.png" style="width:30%; margin-top: 3%; margin-left: 5%; border-radius: 10%; float: left">
+            <div style="margin-left: 50%; margin-top: 3%; margin-right: 10%; font-size: 120%; text-align: center"><b>Roasted Asparagus with Parmesan</b></div>
+            <div style="margin-left: 50%; margin-top: 1%; margin-right: 10%; font-size: 120%; text-align: justify"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, quisquam reprehenderit dolorem provident cupiditate perspiciatis magni voluptatibus libero. Eaque incidunt illo quibusdam commodi est cum quae doloribus rerum nobis cumque!</div>
+
+            <div style="margin-top: 5%; margin-left: 10%"><b>Annonymous</b></div>
+            <i style="margin-left: 10%" class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-half-o"></i>
+            <i class="fa fa-star-o"></i>
+            <div style="margin-top: 1%; margin-left: 10%; margin-right: 50%; text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus harum quasi cupiditate eveniet eos dicta officia qui nemo nostrum, soluta facere sapiente, veniam ipsam consectetur laudantium iste blanditiis at. Eum!</div>
+
+            <div style="text-align: right">
+                <a href="https://fontawesome.com/v4/icons/"><button type="orderFood" style="margin-right: 2%">Order Food</button></a>
+                <div style="float: left; margin-left: 78%">Rp. 34.000,00</div>
+            </div>
+        </form>
+    </div>
 </div>
-
 <script>
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+    function openForm(){
+        document.getElementById("myOverlay").style.display = "block";
+    }
 </script>
 
-</div>
+<script>
+    function closeForm(){
+        document.getElementById("myOverlay").style.display = "none";
+    }
+</script>
+
 @endsection
